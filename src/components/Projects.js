b6717f3c3036b639139aec5cd603128e8bb4c1f0
roshanak <roshanak.ahmed11@gmail.com>
2022-09-07 12:02:43 +0100

    projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,16 +13,16 @@ function Projects() {
           <p className="title is-1 has-text-centered has-text-black">
             Projects
             <Link to="/project-one">
-              <h2>Project One: SNAKE GAME!</h2>
+              <h2>Project One: SNAKE GAME!🐍</h2>
             </Link>
             <Link to="/project-two">
-              <h2>Project Two: Harry Potter Library</h2>
+              <h2>Project Two: Harry Potter Library🪄</h2>
             </Link>
             <Link to="/project-three">
-              <h2>Project Three: I Beg Your Garden?!</h2>
+              <h2>Project Three: I Beg Your Garden?!🪴</h2>
             </Link>
             <Link to="/project-four">
-              <h2>Project Four: Shoe Drip</h2>
+              <h2>Project Four: Shoe Drip👟</h2>
             </Link>
           </p>
         </div>
--- src/components/TheaterArts.js
+++ src/components/TheaterArts.js
@@ -4,7 +4,7 @@ function TheaterArts() {
       <div className="hero-body">
         <div className="container">
           <p className="title is-1 has-text-centered has-text-black">
-            Theater Arts: Acting (2nd grade-11th grade age 9-11. acting, lead actress, improv, script writing, puppet shows, backstage production. Attending plays in Westend)
+            Theater Arts: Acting (2nd grade-11th grade age 9-17. acting, lead actress, improv, script writing, puppet shows, backstage production. Attending plays in Westend)
           </p>
         </div>
       </div>
