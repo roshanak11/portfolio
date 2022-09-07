4e0f4402fd9bb51980e66bd067f9a80764f34f11
roshanak <roshanak.ahmed11@gmail.com>
2022-09-07 11:06:13 +0100

    contact info update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,6 +8,9 @@ function Contact() {
             {/* Links */}
             {/* create form for email */}
             <h2><a href="mailto:roshanak.ahmed11@gmail.com" target="_blank" rel="noopener noreferrer">Email Me</a></h2>
+            <h2><a href="https://www.linkedin.com/in/roshanak11/" target="_blank" rel="noopener noreferrer">LinkedIn</a></h2>
+            <h2><a href="https://github.com/roshanak11/" target="_blank" rel="noopener noreferrer">GitHub</a></h2>
+            <h2><a href="https://instagram.com/rosh_adventures" target="_blank" rel="noopener noreferrer">Instagram</a></h2>
           </p>
         </div>
       </div>
