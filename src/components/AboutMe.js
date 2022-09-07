a9513bb41c0d4489766e31ec5785c9a0e109c55b
roshanak <roshanak.ahmed11@gmail.com>
2022-09-07 15:23:49 +0100

    about me styling + formatting + bulma

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,14 +5,21 @@ function About() {
 
         <div className="card">
           <div className="card-image">
-            <p className="title is-3 has-text-centered has-text-black">
-              <img src="https://res.cloudinary.com/dgicm5dgb/image/upload/v1662556657/Portfolio/passions/me_qizumz_17067d.png" alt="Portrait" width="150" height="300"></img>
-              About Me
-            </p>
-            <h3>
-              My passion for learning, desire to be mentored, growth-mindset, and ability to receive feedback will make me a valuable asset to your industry. I have always been passionate about technology from a young age. I first taught myself how to code when I was 10 years old and won technology awards in school for having the highest score on various projects 5 years in a row. I love using code to construct projects that provide real solutions to real world problems. This is why I enrolled in General Assembly&apos;s Software Engineering Immersive course in London in 2022. In addition, I have a global mindset. I have lived in 3 continents and speak several languages. I am open-minded and I get along with people of all demographics and backgrounds. I would like to join a team that values my skill set and provides a good work-life balance.
-            </h3>
-          </div>
+            <div className="columns">
+              <div className="column has-text-left">
+                <img src="https://res.cloudinary.com/dgicm5dgb/image/upload/v1662556657/Portfolio/passions/me_qizumz_17067d.png" alt="Portrait" width="400" height="400"></img>
+              </div>
+
+              <div className="column has-text-left">
+                <p className="title is-2 has-text-centered has-text-black">
+                  About Me
+                </p>
+                <h3>
+                  My passion for learning, desire to be mentored, growth-mindset, and ability to receive feedback will make me a valuable asset to your industry. I have always been passionate about technology from a young age. I first taught myself how to code when I was 10 years old and won technology awards in school for having the highest score on various projects 5 years in a row. I love using code to construct projects that provide real solutions to real world problems. This is why I enrolled in General Assembly&apos;s Software Engineering Immersive course in London in 2022. In addition, I have a global mindset. I have lived in 3 continents and speak several languages. I am open-minded and I get along with people of all demographics and backgrounds. I would like to join a team that values my skill set and provides a good work-life balance.
+                </h3>
+              </div>
+            </div>
+          </div >
 
           <div className="card">
             <footer className="card-footer">
@@ -23,10 +30,9 @@ function About() {
             </footer>
           </div>
 
-          {/* </div> */}
-        </div>
-      </div>
-    </section>
+        </div >
+      </div >
+    </section >
   )
 }
 
