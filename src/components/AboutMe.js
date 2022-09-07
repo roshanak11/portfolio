30cca6f2df20d3f2f2d393c7eca2ac6b080198bd
roshanak <roshanak.ahmed11@gmail.com>
2022-09-07 01:26:14 +0100

    interests

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -21,7 +21,8 @@ function About() {
             <Link to="/nature-and-exercise">
               <h2>Nature and Exercise</h2>
             </Link>
-            Theater Arts: Acting
+            Theater Arts: Acting (2nd grade-11th grade age 9-11. acting, lead actress, improv, script writing, puppet shows, backstage production)
+            Psychology: Psi Chi Honors Society based on merit, passionate about mental health and the human mind, advocate for equality for all disabilities. Experience tutoring and teaching SpEd and ELL students in middle school and at college-level.
           </p>
         </div>
       </div>
--- src/components/Travel.js
+++ src/components/Travel.js
@@ -5,8 +5,18 @@ function Travel() {
         <div className="container">
           <p className="title is-1 has-text-centered has-text-black">
           Travel:
-          I have lived in 3 continents and traveled to 15 countries, I plan to visit more.
+          I have lived in 3 continents (North America, Europe, and Asia) and traveled to 15 countries, I plan to visit more.
           , learning new languages and learning about different cultures
+          studied french from age 12-20
+          native spoken bengali, studied reading and writing from age 12-17
+          english is my first language
+          also grew up watching bollywood movies in hindi, and hearing urdu in my household. 
+          I took one spanish class in college.
+          I want to travel more and learn more colleges.
+          I have visited India, Bangladesh, Thailand, Singapore, Malaysia, China, Maldives, Hong Kong, UAE, Qatar, Italy, France, England, Mexico, USA
+          Europe: England, Italy, France
+          Asia: Maldives, Singapore, Thailand, Hong Kong, India, Bangladesh, Malaysia, China, Qatar, UAE
+          North America: USA, Mexico
           </p>
         </div>
       </div>
