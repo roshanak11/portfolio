a62e0aeabbd1e4b26808d7e21b173f53c2845c49
roshanak <roshanak.ahmed11@gmail.com>
2022-09-07 14:47:59 +0100

    about me update format

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,26 +1,33 @@
-import { Link } from "react-router-dom"
-import React from "react"
-
 function About() {
   return (
     <section className="hero is-fullheight-with-navbar is-warning">
       <div className="hero-body">
-        <div className="container">
-          <p className="title is-1 has-text-centered has-text-black">
-            About Me
-            Professional Photo (pink turtleneck)
-            Expanded Brand Statement
-            Mention GA enrollment grad SEI
-            {/* Info about industry interests (online resources, podcasts, coding online courses, meetups, etc. GA, Ladies Who Code) */}
-            <h2><a href="https://www.linkedin.com/in/roshanak11/" target="_blank" rel="noopener noreferrer">LinkedIn</a></h2>
-            <h2><a href="https://github.com/roshanak11/" target="_blank" rel="noopener noreferrer">GitHub</a></h2>
-            <h2><a href="https://instagram.com/rosh_adventures" target="_blank" rel="noopener noreferrer">Instagram</a></h2>
-          </p>
-          <p>Learn more about my
-            <Link to="/Passions">
-              <h3>Passions</h3>
-            </Link>
-            outside of work.</p>
+
+        <div className="card">
+          <div className="card-image">
+            <img src="https://res.cloudinary.com/dgicm5dgb/image/upload/v1662556657/Portfolio/passions/me_qizumz_17067d.png" alt="Portrait" width="400" height="200"></img>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-content">
+            <p className="title is-3 has-text-centered has-text-black">
+              About Me
+            </p>
+            <h3>
+              My passion for learning, desire to be mentored, growth-mindset, and ability to receive feedback will make me a valuable asset to your industry. I have always been passionate about technology from a young age. I first taught myself how to code when I was 10 years old and won technology awards in school for having the highest score on various projects 5 years in a row. I love using code to construct projects that provide real solutions to real world problems. I have a global mindset, I have lived in 3 continents and speak several languages. I am open-minded and I get along with people of all demographics and backgrounds. I would like to join a team that values both my skill set and provides a good work-life balance.
+              Mention GA enrollment grad SEI
+            </h3>
+          </div>
+
+          <div className="card">
+            <footer className="card-footer">
+              <a href="https://www.linkedin.com/in/roshanak11/" className="card-footer-item" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+              <a href="https://github.com/roshanak11/" className="card-footer-item" target="_blank" rel="noopener noreferrer">GitHub</a>
+              <a href="https://instagram.com/rosh_adventures" className="card-footer-item" target="_blank" rel="noopener noreferrer">Instagram</a>
+            </footer>
+          </div>
+
         </div>
       </div>
     </section>
